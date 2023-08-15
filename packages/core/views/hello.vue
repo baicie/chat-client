18c4f1a510117eb6e55b0da40db686e726be8106
刘志伟 <2055572879@qq.com>
2023-08-15 16:50:06 +0800

    feat: init

---
+++ packages/core/views/hello.vue
@@ -0,0 +1,16 @@
+<script lang='ts' setup>
+import {IonButton} from '@ionic/vue';
+
+function onButtonFocus(){
+  console.log('onButtonFocus')
+}
+</script>
+
+<template>
+ <div>
+  <div>hellow</div>
+  <IonButton @click="onButtonFocus">IonButton</IonButton>
+ </div>
+</template>
+
+<style lang='scss' scoped></style>
---
+++ packages/web/src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+// import { IonicVue } from '@ionic/vue';
+import './style.css'
+import '@ionic/vue/css/core.css'
+
+import App from './App.vue'
+
+const app = createApp(App)
+// app.use(IonicVue)
+app.mount('#app')
